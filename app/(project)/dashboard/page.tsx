e219ce6e41ae7d3d740fad6fec887968cb587d3b
Felipe Vieira Lima <felipe.lima@inter.co>
2025-04-15 09:18:23 -0300

    feat: init commit

---
+++ app/(project)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboads() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">Dashboard</h1>
+      <p className="text-gray-500">Welcome to the dashboard!</p>
+    </div>
+  )
+}
